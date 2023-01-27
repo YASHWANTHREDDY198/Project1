91230385e779bb1892d84d50ef8fedb4043f8fe2
yashwanth reddy <yashwanthreddy198@gmail.com>
2023-01-27 13:44:02 +1100

    second commit

--- src/class31/Interface.java
+++ src/class31/Interface.java
@@ -3,7 +3,7 @@ package class31;
 interface Interface {
 void m1();
 static void m2(){
-	System.out.println("hi");
+	System.out.println("hi hello ");
 return;
 }
 public static void main(String[] args) {
