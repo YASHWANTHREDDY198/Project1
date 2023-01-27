2a02a50706ced83b99598739eeedd031358a2057
yashwanth reddy <yashwanthreddy198@gmail.com>
2023-01-27 14:13:39 +1100

    get back to older version "second commit"
    This reverts commit 91230385e779bb1892d84d50ef8fedb4043f8fe2.

--- src/class31/Interface.java
+++ src/class31/Interface.java
@@ -3,7 +3,7 @@ package class31;
 interface Interface {
 void m1();
 static void m2(){
-	System.out.println("hi hello ");
+	System.out.println("hi");
 return;
 }
 public static void main(String[] args) {
